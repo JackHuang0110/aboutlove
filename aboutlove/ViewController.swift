116e97c44be9ff478c9b5c790bf5343e14de9e3a
Jack Huang <jackhuang@jackde-macbook-air.local>
2018-08-06 11:54:45 +0800

    new project "About Love"

--- aboutlove/ViewController.swift
+++ aboutlove/ViewController.swift
@@ -9,12 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var myImageView: UIImageView!
+    
+    @IBAction func changeSegmented(_ sender: UISegmentedControl) {
+        
+        if sender.selectedSegmentIndex == 0 {
+            myImageView.image = UIImage(named: "SegmentedLove")
+        }
+        else if sender.selectedSegmentIndex == 1 {
+            myImageView.image = UIImage(named: "SegmentedYou")
+        }
+        else {
+            myImageView.image = UIImage(named: "SegmentedForever")
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    
 }
 
